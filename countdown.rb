dc360c8034c047cac585390ce7d421f3d6ea4fd7
Evans Rutto <robbyevans001@gmail.com>
2022-08-20 16:50:14 +0300

    commit tree cf65a46cd866ee1fe36b8a05c508e071a224f8aa

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  number=int
+  while number>=1
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number-=1
+   
+  end
+  return"HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(int)
+number=int
+while number>0
+  puts "#{number} SECOND(S)!\n"
+  sleep(1)
+  number-=1
+end
+
+return "HAPPY NEW YEAR!"
+end
+
